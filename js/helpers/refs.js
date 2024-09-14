772869e6b925ec323ed3409bacccc7909e79851d
dvankevich <dvankevich@gmail.com>
2024-09-14 13:11:57 +0300

    promices

--- js/helpers/refs.js
+++ js/helpers/refs.js
@@ -1,4 +1,7 @@
 export const refs = {
-    "body":document.querySelector("body"),
-    "checkbox":document.querySelector("#theme-switch")
-}
+  body: document.querySelector('body'),
+  checkbox: document.querySelector('#theme-switch'),
+  ageInput: document.querySelector('.js-input'),
+  ageButton: document.querySelector('.js-form-btn'),
+  ageResult: document.querySelector('.js-form-result'),
+};
--- js/lesson-5.js
+++ js/lesson-5.js
@@ -25,3 +25,84 @@ function renderPage() {
     refs.body.classList.replace('dark', 'light');
   }
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+// const options = {
+//   weekday: 'long',
+//   year: 'numeric',
+//   month: 'long',
+//   day: 'numeric',
+// };
+
+// console.log(new Date().toLocaleDateString('uk-UA', options));
+
+// console.log(new Date().getTime());
+// console.log(new Date().getDay());
+// console.log(new Date().getMonth() + 1);
+
+console.log(Date.now());
+
+refs.ageButton.addEventListener('click', calcalateAge);
+
+function calcalateAge() {
+  const clientDate = new Date(refs.ageInput.value);
+
+  const currentDate = new Date();
+  const diffDate = currentDate - clientDate;
+
+  const result = getAge(diffDate);
+  console.log(result);
+
+  refs.ageResult.textContent = `Your age is ${result} year old`;
+}
+
+function getAge(ms) {
+  return Math.floor(ms / (1000 * 60 * 60 * 24 * 365.25));
+}
+
+/**
+ * - Використовуй prompt і повертай значення звідти.
+ * - Створи функцію, всередині якої буде проміс.
+ * Якщо значення не є числом, відхиляй проміс і логіруй "error".
+ * Якщо значення парне, вирішуй проміс і повертай "even" через 1 секунду.
+ * Якщо значення не парне, вирішуй проміс і повертай "odd" через 2 секунди.
+ */
+
+// console.log('1');
+// setTimeout(() => {
+//   console.log('2');
+// }, 1000);
+
+// setTimeout(() => {
+//   console.log('3');
+// }, 500);
+
+// console.log('4');
+
+const isSuccess = prompt('Enter number');
+
+function checkIsSuccess(value) {
+  return new Promise((resolve, reject) => {
+    if (isNaN(value)) {
+      return reject('Error: enter number');
+    }
+
+    if (value % 2 === 0) {
+      setTimeout(() => {
+        return resolve('Even');
+      }, 1000);
+    }
+
+    setTimeout(() => {
+      return resolve('Odd');
+    }, 2000);
+  });
+}
+
+checkIsSuccess(isSuccess)
+  .then(response => {
+    console.log(response);
+  })
+  .catch(error => {
+    console.log(error);
+  });
