b535a486bb710e67bd0394eb90d9fffa302aca48
Keemply <144055348+Keemply@users.noreply.github.com>
2024-09-14 12:15:49 +0300

    .

--- js/lesson-5.js
+++ js/lesson-5.js
@@ -1,19 +1,27 @@
-import { refs } from "./helpers/refs.js";
- import {addDataToLocalSrtorag} from "./helpers/localStorage-ip.js";
-
-refs.checkbox.addEventListener("click", onClick);
+import { refs } from './helpers/refs.js';
+import {
+  addDataToLocalSrtorage,
+  getDataFromLocalStorage,
+} from './helpers/localStorage-ip.js';
+import { LS_k } from './helpers/keys.js';
+document.addEventListener('DOMContentLoaded', renderPage);
+refs.checkbox.addEventListener('click', onClick);
 
 function onClick(event) {
-    console.log(event.target.checked);
-    if (event.target.checked){
-        refs.body.classList.replace("light", "dark");
-        
-    }
-    else {
-        refs.body.classList.replace("dark", "light");
-          }
-    
-          
-        
-
+  console.log(event.target.checked);
+  if (event.target.checked) {
+    refs.body.classList.replace('light', 'dark');
+    addDataToLocalSrtorage(LS_k, 'dark');
+  } else {
+    refs.body.classList.replace('dark', 'light');
+    addDataToLocalSrtorage(LS_k, 'light');
+  }
+}
+function renderPage() {
+  const lsData = getDataFromLocalStorage(LS_k);
+  if (lsData === 'dark') {
+    refs.body.classList.replace('light', 'dark');
+  } else {
+    refs.body.classList.replace('dark', 'light');
+  }
 }
